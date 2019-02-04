3514f59f5f43db571a80cf0e1aebb46faa5a00a8
sakshitaneja2227 <sakshitaneja2227@gmail.com>
2019-02-04 12:30:18 +0530

    Afdding basic testNg functionalities

---
+++ testNg/src/test/java/PracticeClass.java
@@ -0,0 +1,31 @@
+
+
+import org.testng.annotations.Test;
+
+public class PracticeClass {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Inside Test1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Inside Test2");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Inside Test3");
+	}
+
+	@Test
+	public void test41()
+	{
+		System.out.println("Inside Test41");
+	}
+	
+}
